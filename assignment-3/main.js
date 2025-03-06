12a178525d9d1aaa7d0ad8e039ab4f26a67532d5
marwa-04 <marwaelazzazi@gmail.com>
2025-03-06 17:10:06 +0200

    third

---
+++ assignment-3/main.js
@@ -0,0 +1,28 @@
+let numberOne = 10;
+let numberTwo = 20;
+
+console.log(numberOne +""+ numberTwo);
+console.log(typeof (numberOne +""+ typeof numberTwo));
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof(`${numberOne}${numberTwo}`));
+
+
+console.log(numberOne +"\n"+ numberTwo);
+
+console.log(`${numberOne}
+${numberTwo}`);
+
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+console.log(`\`I'm In \n
+\\\\ \n
+Love \\\\ \"\"\" \'\'\' \n
+\+\+ With \+\+
+\\\"\"\"\\\"\"\" \n
+\"\"JavaScript\"\" \`\``)
+
+let a = 21;
+let b = 20;
+
+console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}`); // _21_2021_2021_2021_20_
